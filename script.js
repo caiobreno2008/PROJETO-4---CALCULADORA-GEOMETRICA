03835eaae1397f311971353e06d4c8ecf9a02de3
caiobreno2008 <caiobreno155@gmail.com>
2024-11-07 09:40:15 -0300

    Update script.js
    codigo versao ingles

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 function menu() {
-  let opcao = prompt("Escolha uma opção: \n 1 - Área do Triângulo \n 2 - Área do Círculo \n 3 - Área do Quadrado \n 4 - Área do Retângulo \n 5 - Sair");
+  let option = prompt("Escolha uma opção: \n 1 - Área do Triângulo \n 2 - Área do Círculo \n 3 - Área do Quadrado \n 4 - Área do Retângulo \n 5 - Sair")
 
-  switch(opcao) {
+  switch(option) {
     case '1':
-      let baseTriangulo = parseFloat(prompt("Digite a base do triângulo:"));
-      let alturaTriangulo = parseFloat(prompt("Digite a altura do triângulo:"));
-      alert("A área do triângulo é: " + areaTriangulo(baseTriangulo, alturaTriangulo));
-      break;
+      let baseTriangle = parseFloat(prompt("Digite a base do triângulo:"))
+      let heightTriangle = parseFloat(prompt("Digite a altura do triângulo:"))
+      alert("A área do triângulo é: " + areaTriangle(baseTriangle, heightTriangle))
+      break
     case '2':
-      let raio = parseFloat(prompt("Digite o raio do círculo:"));
-      alert("A área do círculo é: " + areaCirculo(raio));
-      break;
+      let radius = parseFloat(prompt("Digite o raio do círculo:"))
+      alert("A área do círculo é: " + areaCircle(radius))
+      break
     case '3':
-      let ladoQuadrado = parseFloat(prompt("Digite o lado do quadrado:"));
-      alert("A área do quadrado é: " + areaQuadrado(ladoQuadrado));
-      break;
+      let sideSquare = parseFloat(prompt("Digite o lado do quadrado:"))
+      alert("A área do quadrado é: " + areaSquare(sideSquare))
+      break
     case '4':
-      let baseRetangulo = parseFloat(prompt("Digite a base do retângulo:"));
-      let alturaRetangulo = parseFloat(prompt("Digite a altura do retângulo:"));
-      alert("A área do retângulo é: " + areaRetangulo(baseRetangulo, alturaRetangulo));
-      break;
+      let baseRectangle = parseFloat(prompt("Digite a base do retângulo:"))
+      let heightRectangle = parseFloat(prompt("Digite a altura do retângulo:"))
+      alert("A área do retângulo é: " + areaRectangle(baseRectangle, heightRectangle))
+      break
     case '5':
-      sair();
-      break;
+      exit()
+      break
     default:
-      alert("Opção inválida!");
-      break;
+      alert("Opção inválida!")
+      break
   }
 }
 
-function areaTriangulo(base, altura) {
-  let area = (base * altura) / 2;
-  return area;
+function areaTriangle(base, height) {
+  let area = (base * height) / 2
+  return area
 }
 
-function areaCirculo(raio) {
-  let area = 3.14 * (raio * raio);
-  return area;
+function areaCircle(radius) {
+  let area = 3.14 * (radius * radius)
+  return area
 }
 
-function areaQuadrado(lado) {
-  let area = lado * lado;
-  return area;
+function areaSquare(side) {
+  let area = side * side
+  return area
 }
 
-function areaRetangulo(base, altura) {
-  let area = base * altura;
-  return area;
+function areaRectangle(base, height) {
+  let area = base * height
+  return area
 }
 
-function sair() {
-  console.log("Saindo...");
-  alert("Saindo...");
+function exit() {
+  console.log("Saindo...")
+  alert("Saindo...")
 }
 
-
-menu();
+menu()
